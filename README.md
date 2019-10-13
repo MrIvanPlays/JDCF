774b744e13fc97234c2d77b902874abd1a5a3f46
MrIvanPlays <pekov.ivan@abv.bg>
2019-10-13 19:30:07 +0300

    Javadocs on the RegisteredCommand class, also license and README

---
+++ README.md
@@ -0,0 +1,15 @@
+![license](https://img.shields.io/github/license/MrIvanPlays/JDCF.svg?style=for-the-badge)
+![issues](https://img.shields.io/github/issues/MrIvanPlays/JDCF.svg?style=for-the-badge)
+[![support](https://img.shields.io/discord/493674712334073878.svg?colorB=Blue&logo=discord&label=Support&style=for-the-badge)](https://mrivanplays.com/discord)
+
+# JDCF
+
+A command framework for the famous discord api for java - ![JDA](https://github.com/DV8FromTheWorld/JDA)
+
+## Installation
+
+todo
+
+## Usage
+
+todo
--- src/main/java/com/mrivanplays/jdcf/RegisteredCommand.java
+++ src/main/java/com/mrivanplays/jdcf/RegisteredCommand.java
@@ -24,7 +24,15 @@ package com.mrivanplays.jdcf;
 
 import com.mrivanplays.jdcf.args.CommandArguments;
 import net.dv8tion.jda.api.Permission;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents a registered command. A registered command is a command that was
+ * registered into a command manager. In order to have easy access to all the
+ * data for the command, this class takes in place, storing all the things
+ * possible for a command.
+ */
 public final class RegisteredCommand
 {
 
@@ -41,31 +49,67 @@ public final class RegisteredCommand
         this.aliases = aliases;
     }
 
+    /**
+     * Returns the name of the command.
+     *
+     * @return name
+     */
+    @NotNull
     public String getName()
     {
         return command.getName();
     }
 
+    /**
+     * Returns all the permissions of the command (if specified)
+     *
+     * @return permissions
+     */
+    @Nullable
     public Permission[] getPermissions()
     {
         return command.getPermissions();
     }
 
+    /**
+     * Returns the usage of the command (if specified)
+     *
+     * @return usage
+     */
+    @Nullable
     public String getUsage()
     {
         return usage;
     }
 
+    /**
+     * Returns the description of the command (if specified)
+     *
+     * @return description
+     */
+    @Nullable
     public String getDescription()
     {
         return description;
     }
 
+    /**
+     * Returns the aliases of the command (if specified)
+     *
+     * @return aliases
+     */
+    @Nullable
     public String[] getAliases()
     {
         return aliases;
     }
 
+    /**
+     * Executes the command with the specified context and arguments.
+     *
+     * @param context context
+     * @param args    arguments
+     */
     public void execute(CommandExecutionContext context, CommandArguments args)
     {
         command.execute(context, args);
