a957f478b8abff2b6f384f13bcb99b7c63f4573a
MrIvanPlays <pekov.ivan@abv.bg>
2020-04-10 12:10:14 +0300

    Simplify value specification

--- src/main/java/com/mrivanplays/jdcf/args/ArgumentResolverResult.java
+++ src/main/java/com/mrivanplays/jdcf/args/ArgumentResolverResult.java
@@ -10,12 +10,7 @@ public interface ArgumentResolverResult<T, D> {
         if (value == null && failureReason == null) {
             throw new IllegalArgumentException("Parsed value is null but there's no failure reason.");
         }
-        Optional<T> valueOptional;
-        if (value == null) {
-            valueOptional = Optional.empty();
-        } else {
-            valueOptional = Optional.of(value);
-        }
+        Optional<T> valueOptional = value == null ? Optional.empty() : Optional.of(value);
         return new ArgumentResolverResult<T, D>() {
             @Override
             public Optional<T> getValue() {
